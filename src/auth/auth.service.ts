864b4c08a3d5efcf2abd97d7c94d7ed427c5dc6b
manulangat <emmanuelthedeveloper@gmail.com>
2022-07-03 13:57:39 +0300

    feat set up prisma configurations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+// import { User, Bookmark, PrismaClient } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   login() {
-    return 'Hi there!';
+    return 'Hi there!, you are trying to login in using post oops!';
   }
   logout() {}
   signup() {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://dev:dev@localhost:5434/devnest?schema=public',
+        },
+      },
+    });
+  }
+}
