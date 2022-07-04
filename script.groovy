a279756eb1520ae4e807d1af09d6c300035e2d1e
manulangat <emmanuelthedeveloper@gmail.com>
2022-07-04 22:49:09 +0300

    feat-jenkins-file-added

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def buildApp () { 
     echo "building application"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable:'pass',usernameVariable:'USER'])){
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable:'pass',usernameVariable:'USER')]){
         sh 'docker build -t manulangat/nest-docker-practise:1.0'
         sh  "echo $PASS | docker login -u $USER --password-stdin "
         sh "docker push manulangat/nest-docker-practise:1.0"
