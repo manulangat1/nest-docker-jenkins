f40049913c82b27baf1ad3ab0e00616b2a47ec9d
manulangat <emmanuelthedeveloper@gmail.com>
2022-07-16 15:56:03 +0300

    .

--- server-commands.sh
+++ server-commands.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash 
 
 echo "starting server"
+export IMAGE=$1
 docker-compose -f docker-compose.prod.yml down --volumes --remove-orphans 
 docker-compose -f docker-compose.prod.yml up --detach   --remove-orphans 
 echo "success"
